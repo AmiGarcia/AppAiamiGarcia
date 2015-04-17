7344544c39663f40778035b2597b33222acbcdea
AmiHoppus182 <aiami@live.com>
2015-04-17 14:19:40 -0300

    Initial Commit

---
+++ Aiami-GarciaTests/Aiami_GarciaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Aiami_GarciaTests.swift
+//  Aiami-GarciaTests
+//
+//  Created by Ami Garcia on 4/17/15.
+//  Copyright (c) 2015 Ami Garcia. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Aiami_GarciaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
